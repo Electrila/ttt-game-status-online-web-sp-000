b6a294b89004a4e2248c266d3ea963bc42f3d087
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-17 13:46:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,3 +26,11 @@ def won?(board)
     position_3 = board[win_index_3]
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      win_combo
+    else
+      false
+    end
+  end
+end
